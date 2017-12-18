347d7ee1e34e302bff9906e28df3666465bab048
Mark Deering <m.deering@yahoo.com>
2017-12-18 15:37:31 -0600

    created components

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { LogRegComponent } from './log-reg/log-reg.component';
+import { MessageboardComponent } from './home/messageboard/messageboard.component';
+import { SearchbarComponent } from './home/searchbar/searchbar.component';
+import { ProfileComponent } from './home/profile/profile.component';
+import { CommentComponent } from './home/comment/comment.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LogRegComponent
+  },
+  {
+    path: 'messages',
+    component: HomeComponent,
+    children:[]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
